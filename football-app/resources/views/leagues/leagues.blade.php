0a3ae9db4893a740bedd9a3ce60bd273d4c6fe88
Nacho Albiol <nacho.albiol88@gmail.com>
2024-05-02 13:35:30 +0200

    First approach Create Leagues

--- football-app/resources/views/leagues/leagues.blade.php
+++ football-app/resources/views/leagues/leagues.blade.php
@@ -5,7 +5,7 @@
         Get started creating a <span class="text-teal-300">New League</span></h1>
     <p class="text-lg font-normal text-gray-200 lg:text-xl">Do you want to create a new League? With League Manager is
         possible, just press the button and follow the instructions.</p>
-    <a href="{{ route('league.create') }}"
+    <a href="{{ route('leagues.create') }}"
         class="relative inline-flex items-center justify-center p-0.5 my-4 me-2 overflow-hidden text-sm font-medium text-white rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white focus:ring-4 focus:outline-none focus:ring-green-400">
         <span
             class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-gray-900 rounded-md group-hover:bg-opacity-0">
@@ -14,7 +14,6 @@
     </a>
 
     <h1 class="my-6 text-3xl font-extrabold leading-none tracking-tight text-white md:text-4xl lg:text-5xl">Teams List</h1>
-
 
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table class="w-full text-sm text-left rtl:text-right text-gray-400">
