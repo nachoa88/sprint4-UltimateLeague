faf7aa253b65b84977cfae5d181c203772090df0
Nacho Albiol <nacho.albiol88@gmail.com>
2024-05-07 18:32:51 +0200

    Small fix in layout

--- football-app/resources/views/layouts/app.blade.php
+++ football-app/resources/views/layouts/app.blade.php
@@ -76,7 +76,7 @@
         <div class="mx-auto w-full max-w-screen-xl p-4">
             <div class="md:flex md:justify-between">
                 <div class="flex items-center">
-                    <a href="https://flowbite.com/" class="flex items-center">
+                    <a href="#" class="flex items-center">
                         <img src="{{ asset('images/iap-logo.png') }}" class="h-12 me-3" alt="IAP-Dev Logo" />
                     </a>
                 </div>
@@ -85,10 +85,10 @@
                         <h2 class="mb-6 text-sm font-semibold text-white uppercase">About</h2>
                         <ul class="text-gray-400 font-medium">
                             <li class="mb-4">
-                                <a href="https://github.com/nachoa88" class="hover:underline ">Github</a>
+                                <a href="https://github.com/nachoa88" target="_blank" class="hover:underline ">Github</a>
                             </li>
                             <li>
-                                <a href="https://ignacioalbiol.tech/" class="hover:underline">Portfolio</a>
+                                <a href="https://ignacioalbiol.tech/" target="_blank" class="hover:underline">Portfolio</a>
                             </li>
                         </ul>
                     </div>
@@ -108,7 +108,7 @@
             <hr class="my-2 border-gray-600 sm:mx-auto lg:my-4" />
             <div class="sm:flex sm:items-center sm:justify-between">
                 <span class="text-sm text-gray-400 sm:text-center">© 2024 <a
-                        href="https://ignacioalbiol.tech/" class="hover:underline">IAP-Dev</a>. All Rights Reserved.
+                        href="https://ignacioalbiol.tech/" target="_blank" class="hover:underline">IAP-Dev</a>. All Rights Reserved.
                 </span>
             </div>
         </div>
