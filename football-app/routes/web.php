c6515f228a0cf4f7f907370114dc5eb6e5de000d
Nacho Albiol <nacho.albiol88@gmail.com>
2024-05-10 11:29:04 +0200

    Working base Breeze setup

--- football-app/routes/web.php
+++ football-app/routes/web.php
@@ -3,10 +3,31 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TeamsController;
+use App\Http\Controllers\GameController;
+use App\Http\Controllers\LeagueController;
+
+// IMPORTANT: The specific routes must go before than the resource one.
+// I've added the HomeController to return the view instead of the anonymous function.
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// Added two routes to recover the teams of a league that have been deleted.
+Route::get('teams/deleted', [TeamsController::class, 'showDeleted'])->name('teams.deleted');
+Route::put('teams/{team}/restore', [TeamsController::class, 'restore'])->name('teams.restore');
+// Route for the select-league form, to store the league_id in the session.
+Route::post('/select-league', [LeagueController::class, 'selectLeague'])->name('select-league');
+
+// With resouce method, we can create all the routes for the controller with one line of code.
+Route::resource('teams', TeamsController::class);
+Route::resource('games', GameController::class);
+Route::resource('leagues', LeagueController::class);
+
+/*
 Route::get('/', function () {
     return view('welcome');
 });
-
+*/
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -18,3 +39,6 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+
